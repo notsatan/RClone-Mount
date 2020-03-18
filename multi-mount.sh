fd7d5f6a7936fdae6f0a4c37c38fe2395ccd20bb
Demon Rem <22884507+demon-rem@users.noreply.github.com>
2020-03-18 13:42:00 +0530

    Update: Improve logic to detect arguments.
    Change the logic to detect arguments supplied to the script. Accept
    additional 'force' argument.
    
    Modify comments to improve overall readibility (hopefully).

--- multi-mount.sh
+++ multi-mount.sh
@@ -4,30 +4,42 @@
 # root directory. All the configs regardless of their type will be mounted by this script.
 
 # Location of the root directory. A separate directory will be created to use as a mount point
-# for each RClone config inside the root directory. If you want to hard-code the location of
-# the root directory, add its location as the value of the variable below.
+# for each RClone config inside the root directory. A valid path can be hard-coded as the value
+# of the variable if required.
 root_dir=""
 
 # The flag that should be used while passing the root directory argument to use
 # the directory as the root directory.
 DIR_FLAG="--root="
 
+# Force flag, will be used to indicate that the value is forced.
+FORCE_ARGUMENT="--force"
+
+# Will be used to determine if the process of mounting drives to directories should be forced.
+# Should contain a boolean value, can be hard-coded with a default value if required. Initially
+# setting the value of this variable to be an empty string.
+FORCE=""
+
+# Iterating over each argument supplied, and using the argument if it is valid.
+for argument in "${@}"; do
+	if [[ $argument == $DIR_FLAG* && -z $root_dir ]]; then
+		# If the root directory is not set already and the argument starts with the flag for root
+		# directory, replacing the flag from the string and taking the rest as the root directory.
+		root_dir=$(echo ${argument} | sed "s/${DIR_FLAG}//g")
+	elif [[ -z $FORCE && ("${argument,,}" == "${FORCE_ARGUMENT,,}" || "${argument}" == "-F") ]]; then
+		# If the value of force is blank string (same as default), and if the force argument is
+		# supplied, setting the value of the variable to be true.
+		FORCE=true
+	fi
+done
+
+if [[ -z $FORCE ]]; then
+	# If the value of force is still an empty string, setting it to be false.
+	FORCE=false
+fi
+
 # Ensuring that the directory being used as root actually exists.
 while true; do
-	# Checking if an argument has been supplied while running the command, and if an input
-	# has been supplied, checking if the flag is the same as the root. If the flag is not
-	# the same, then the path supplied will not be treated as the root directory.
-	if [[ ! -z $1 && $1 == *$DIR_FLAG* ]]; then
-		# If input is supplied, getting the value by replacing the flag with an empty string
-		# and taking the rest of the value as the value supplied for the root directory.
-		root_dir=$(echo ${1} | sed "s/${DIR_FLAG}//g")
-
-		# Once the argument is read, changing it to be an empty string. This ensures if the
-		# path supplied as argument is invalid, the script does not enter into an infinite
-		# loop where it will read the argument every time and print that the path is invalid.
-		set -- "" # This statement is equivalent to $1="" and works :p
-	fi
-
 	if [[ -z "${root_dir}" ]]; then
 		# If root directory is not set (the value has neither been hard-coded nor been supplied
 		# as an argument), getting the value from the user.
@@ -36,11 +48,10 @@ while true; do
 
 	# Validating if the path supplied as the root directory is valid or not.
 	if [[ ! -d $root_dir ]]; then
-		# Flow of control reaches here only if the path is invalid.
+		# Flow of control reaches here only if the path is not valid.
 		printf "\nPath supplied as the root directory '${root_dir}' is invalid \n\n"
 
-		# Empty the value of `root_dir` to ensure that the user is made to re-enter this value in
-		# the next iteration of the loop.
+		# Empty the value of `root_dir` to ask the user is to re-enter this value in the next iteration.
 		root_dir=""
 	else
 		# Converting relative path to absolute path, if the path supplied is the absolute path,
@@ -68,11 +79,11 @@ while true; do
 		read -p "Enter full path for the RClone config file: " config
 	fi
 
-	# If the path is valid, converting relative path to absolute.
+	# If the path is a valid to a file, converting the relative path to absolute.
 	if [[ -f "${config}" ]]; then
 		config=$(realpath "${config}")
 
-		# If the converted path is valid, breaking out of the infinite loop.
+		# If the converted path points to a file, breaking out of the infinite loop.
 		if [[ -f $config ]]; then
 			break
 		else
@@ -87,7 +98,7 @@ cut -f2 "${config}" | while read line; do
 
 	# Reading the line and extracting the part that is present inside the box-braces.
 	# The regex-string is selected such that the line should start with box bracket and
-	# end at box bracket. Anything between these brackets will be read and treated as
+	# end with a box bracket. Anything between these brackets will be read and treated as
 	# the name of the config. And if the line does not contain the name of the config,
 	# then `config_name` will be an empty variable.
 	config_name=$(echo "${line}" | grep -o '\[.*\]$')
@@ -95,23 +106,23 @@ cut -f2 "${config}" | while read line; do
 	if [[ ! -z "${config_name}" ]]; then
 		# If the name of the config is not empty (indicating that the value in the particular
 		# line is the name of the config), then extracting the actual name of the config.
-		# Replacing the starting and ending box-bracket with nothing.
+		# Removing the brackets by replacing them with nothing.
 		config_name=$(echo "${config_name}" | sed "s/[][]//g")
 
 		# Converting the name of the config to the name of directory inside the root directory.
 		# Since the name of the directory is to be made using the name of the config, replacing
-		# colon, back-slash and forward-slashes (if any) with a space(s).
-		dir_name=$(echo "${config_name}" | sed 's.\\. .g; s./. .g; s.:. .g')
-		# For Linux, the name of a directory can't contain forward-slash, however, as I'm on
-		# Dual Boot currently (with the other OS being Windows),so I've veto-ed the decision to
-		# forbid backslash and colons from the directory name too :p
-		# Anyone who wants to remove this restriction is free to make a fork without these restrictions.
+		# all characters that are not allowed for directory names in Windows.
+		dir_name=$(echo "${config_name}" | sed 's.\\. .g; s./. .g; s.:. .g; s.". .g; s.|. .g')
+		# For Linux, the a directory name can't contain forward-slash, however, as I'm on
+		# Dual Boot currently (with the other OS being Windows), so I've veto-ed the decision to
+		# forbid any characters that are not allowed in Windows.
+		# Anyone who doesn't want this can make the changes to a fork or to their local copy.
 
 		# Changing the directory to the root directory.
 		cd "${root_dir}"
 
-		# Creating a directory for the config inside root directory, and suppressing any error(s)
-		# by redirecting STDERR to '/dev/null'
+		# Creating a directory for the config, and suppressing any error(s) by redirecting
+		# STDERR to '/dev/null'
 		output=$(mkdir "${dir_name}" -v 2>/dev/null)
 
 		if [[ ! -z "${output}" && "${output}" == *"created directory"* ]]; then
