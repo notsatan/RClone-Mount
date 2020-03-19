bc0bd12ed95e23f5ab60cf2fa5be61a06f9f4840
Demon Rem <22884507+demon-rem@users.noreply.github.com>
2020-03-19 11:05:00 +0530

    Feature: Complete a basic working version of the script
    Add code to create a new text file (or wipe out the existing file),
    this text file will contain the full paths of the directories
    where RClone configs are mounted by this script.
    
    The data from this file will be used to create a script to unmount
    all the mounted configs.
    
    Add code to create a new directory inside the root directory for every
    config and mount the config to the corresponding directory.
    
    Minor improvements in the comments to better explain the working of the
    script. Also, create a string containing a few hard-coded arguments that
    will be used while mounting the config(s).

--- multi-mount.sh
+++ multi-mount.sh
@@ -20,6 +20,25 @@ FORCE_ARGUMENT="--force"
 # setting the value of this variable to be an empty string.
 FORCE=""
 
+# THe name of the text file that will contain the ID of the background processes running RClone,
+# and the directories that have been mounted.
+MOUNT_FILE="mount.txt"
+
+# The signing signature, will be present at the top of the mount-file.
+FILE_SIGNATURE=("RClone-Mount v0.3" "Do not make any modifications to this file")
+
+# String that will contain the flags that are to be use while mounting directories with RClone.
+MOUNT_FLAGS="--cache-db-purge --buffer-size 256M --drive-chunk-size 32M --vfs-cache-mode minimal 
+			--vfs-read-chunk-size 128M --vfs-read-chunk-size-limit 1G"
+#
+# Brief usage for these flags:
+# 	--cache-db-purge	 		: Will purge the previous cache.
+# 	--buffer-size		 		: The amount of memory used to buffer data in advance.
+#	--drive-chunk-size			: The minimum size of a chunk while uploading files.
+# 	--vfs-cache-mode			: Balances upload/download with regards to resource consumption.
+#	--vfs-read-chunk-size		: The size of chunks while reading.
+#
+
 # Iterating over each argument supplied, and using the argument if it is valid.
 for argument in "${@}"; do
 	if [[ $argument == $DIR_FLAG* && -z $root_dir ]]; then
@@ -47,7 +66,7 @@ while true; do
 	fi
 
 	# Validating if the path supplied as the root directory is valid or not.
-	if [[ ! -d $root_dir ]]; then
+	if [[ ! -d "${root_dir}" ]]; then
 		# Flow of control reaches here only if the path is not valid.
 		printf "\nPath supplied as the root directory '${root_dir}' is invalid \n\n"
 
@@ -59,7 +78,7 @@ while true; do
 		root_dir=$(realpath "${root_dir}")
 
 		# Again, validating that the absolute path exists.
-		if [[ -d $root_dir ]]; then
+		if [[ -d "${root_dir}" ]]; then
 			# If the path is still valid, breaking out of the infinite loop.
 			break
 		fi
@@ -93,6 +112,16 @@ while true; do
 	fi
 done
 
+# Creating the mount file. This file will be used to contain the paths of the directories
+# to which these RClone configs are mounted.
+# Starting by adding a blank character to the file, this will create the file or wipe all
+# the file if it already exists.
+printf "" >"${MOUNT_FILE}"
+for string in "${FILE_SIGNATURE[@]}"; do
+	# Adding the file signature to the top of the file.
+	echo "${string}" >>"${MOUNT_FILE}"
+done
+
 # Reading the contents of the config file line-by-line.
 cut -f2 "${config}" | while read line; do
 
@@ -104,35 +133,49 @@ cut -f2 "${config}" | while read line; do
 	config_name=$(echo "${line}" | grep -o '\[.*\]$')
 
 	if [[ ! -z "${config_name}" ]]; then
-		# If the name of the config is not empty (indicating that the value in the particular
-		# line is the name of the config), then extracting the actual name of the config.
-		# Removing the brackets by replacing them with nothing.
-		config_name=$(echo "${config_name}" | sed "s/[][]//g")
-
-		# Converting the name of the config to the name of directory inside the root directory.
-		# Since the name of the directory is to be made using the name of the config, replacing
-		# all characters that are not allowed for directory names in Windows.
-		dir_name=$(echo "${config_name}" | sed 's.\\. .g; s./. .g; s.:. .g; s.". .g; s.|. .g')
-		# For Linux, the a directory name can't contain forward-slash, however, as I'm on
-		# Dual Boot currently (with the other OS being Windows), so I've veto-ed the decision to
-		# forbid any characters that are not allowed in Windows.
-		# Anyone who doesn't want this can make the changes to a fork or to their local copy.
-
-		# Changing the directory to the root directory.
-		cd "${root_dir}"
-
-		# Creating a directory for the config, and suppressing any error(s) by redirecting
-		# STDERR to '/dev/null'
-		output=$(mkdir "${dir_name}" -v 2>/dev/null)
-
-		if [[ ! -z "${output}" && "${output}" == *"created directory"* ]]; then
-			# If the output contains the string 'created directory', this indicates that the directory
-			# was created successfully.
-			echo "Created directory ${dir_name}"
-		else
-			# Since the error is being redirected to '/dev/null', in case of any error, the output
-			# will be an empty string.
-			echo "Failed to create directory ${dir_name}"
-		fi
+		# If the name of the config is empty (indicating that the value in the particular
+		# line is not the name of a config), jumping to the next line in the file.
+		continue
 	fi
+
+	# If the line contains the name of a valid config, removing the box brackets at the
+	# beginning and ending of the string by replacing them.
+	config_name=$(echo "${config_name}" | sed "s/[][]//g")
+
+	# Converting the name of the config to the name of directory inside the root directory.
+	# Since the name of the directory is to be made using the name of the config, replacing
+	# all characters that are not allowed for directory names in Windows.
+	dir_name=$(echo "${config_name}" | sed 's.\\. .g; s./. .g; s.:. .g; s.". .g; s.|. .g')
+	# For Linux, the a directory name can't contain forward-slash, however, as I'm on
+	# Dual Boot currently (with the other OS being Windows), so I've veto-ed the decision to
+	# forbid any characters that are not allowed in Windows.
+	# Anyone who doesn't want this can make the changes to a fork or to their local copy.
+
+	# Creating a directory for the config inside the root directory, and suppressing any
+	# error(s) by redirecting STDERR to '/dev/null'. Also, the verbose flag is important
+	# and will be used to verify that the directory was created successfully.
+	output=$(mkdir "${root_dir}/${dir_name}" -v 2>/dev/null)
+
+	if [[ -z "${output}" && "${output}" != *"created directory"* ]]; then
+		# Since the error is being redirected to '/dev/null', in case of any error, the output
+		# will be an empty string or if the output does not contain the string 'created directory',
+		# the directory could not be created.
+		echo "Failed to create directory ${dir_name}"
+	fi
+
+	# If the root directory exists, mounting it with the added flags.
+	if [[ -d "${root_dir}" ]]; then
+		# Once the directory is created, mounting the config to the created directory in a
+		# background process.
+		rclone mount "${config_name}": "${root_dir}/${dir_name}" $MOUNT_FLAGS &
+		# The '&' part at the end of the command, starts a background process instead of
+		# blocking the terminal window.
+
+		# Adding the mount path as a string to the mount file.
+		echo "${root_dir}/${dir_name}" >>"./${MOUNT_FILE}"
+
+		# Printing a message to inform that the config was mounted successfully.
+		echo "Mounted config [${config_name}]"
+	fi
+
 done
